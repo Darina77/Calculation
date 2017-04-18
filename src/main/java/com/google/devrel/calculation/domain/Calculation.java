db3ad45900846a0d26420eea51cc56b8e1d16e47
Darina77 <darina.pishchik@gmail.com>
2017-04-18 22:02:31 +0300

    Revert "Add recipe interface and divide recipes"
    This reverts commit 8d20740487d7de743f9e531048a79ef99f27d03e.

--- src/main/java/com/google/devrel/calculation/domain/Calculation.java
+++ src/main/java/com/google/devrel/calculation/domain/Calculation.java
@@ -8,25 +8,14 @@ public class Calculation
 
     private Recipe recipe;
     private double onePortionWeight; // грами
-    private static final double PDV = 0.2;
-    private double extra; // відсотки поділені на 100
 
     public Calculation(Recipe recipe){
         this.recipe = recipe;
         onePortionWeight = 100;
-        extra = 1;
     }
 
-    public Calculation(Recipe recipe, double onePortionWeight, double extra) {
+    public Calculation(Recipe recipe, double onePortionWeight) {
         this.recipe = recipe;
         this.onePortionWeight = onePortionWeight;
-        this.extra = extra;
     }
-
-    //todo
-    public double calculate(){
-        return 0;
-    }
-
-
 }
--- src/main/java/com/google/devrel/calculation/domain/Recipe.java
+++ src/main/java/com/google/devrel/calculation/domain/Recipe.java
@@ -2,18 +2,96 @@ package com.google.devrel.calculation.domain;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 /**
- * Created by Darina on 18.04.2017.
+ * Рецепти страв
+ * Нічого кращого я не придумала
+ * @author  Darina
  */
-public interface Recipe
+public class Recipe
 {
-     Map<Product, Double> ingredients = new HashMap<>(); // продукти та норма в порціях або в
-                                                       // кг відповідно ціна одиниці товару (за кг або за штуку)
-     void add(Product product, double norm);
-     void delete(Product product);
-     double getNorm(Product product);
-     double getSum(Product product);
-     String getName();
-     int getRecipeId();
+    private String name;
+    private int recipeId;
+    private Map<Product, Double> ingredients; // продукти та іх необхідна норма (вага в кг)
+
+
+    /**
+     * Конструктор рецептів
+     * @param recipeId id рецепта
+     * @param name назва рецепта
+     */
+    public Recipe(int recipeId, String name)
+    {
+        this.recipeId = recipeId;
+        this.name = name;
+        ingredients = new HashMap<>();
+    }
+
+    /**
+     * Друга варіація конструктору
+     * @param recipeId id рецепта
+     * @param name назва рецепта
+     * @param ingredients набір інгрідієнтів
+     */
+    public Recipe(int recipeId, String name, Map<Product, Double> ingredients)
+    {
+        this.recipeId = recipeId;
+        this.name = name;
+        this.ingredients = ingredients;
+    }
+
+    /**
+     * Додавання нового інгрідієнту в рецепт
+     * @param product продукт
+     * @param norm норма продкуту (кількість в кг)
+     */
+    public void add(Product product, Double norm)
+    {
+        if (product == null || norm == null) throw  new NullPointerException();
+        ingredients.put(product, norm);
+    }
+
+    /**
+     * Видалення продукту з рецепту
+     * @param product продукт який необхідно видалити
+     */
+    public void delete(Product product)
+    {
+        if (product == null) throw new NullPointerException();
+        if (ingredients.containsKey(product))
+        {
+            ingredients.remove(product);
+        }
+        else throw new NoSuchElementException();
+    }
+
+    /**
+     * @param product для якого продукту необхідно взяти норму з рецепту
+     * @return норму продукту в цьому рецепті
+     */
+    public double getNorm(Product product)
+    {
+        if (product == null) throw new NullPointerException();
+
+        if (ingredients.containsKey(product))
+            return ingredients.get(product);
+
+        else throw new NoSuchElementException();
+    }
+
+    /**
+     * @param product для якого продукту визначити сумму
+     * @return сумму грошей за всю вагу продукту
+     */
+    public double getSum(Product product)
+    {
+        if (product == null) throw new NullPointerException();
+
+        if (ingredients.containsKey(product))
+            return product.sum(ingredients.get(product));
+
+        else throw new NoSuchElementException();
+    }
+
 }
