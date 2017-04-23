cb3300267daa39d4d32589716080ebb0575a5b81
Solomia2018 <solomia2018@gmail.com>
2017-04-23 11:46:45 +0300

    "Validation"

--- src/main/webapp/js/FormJS.js
+++ src/main/webapp/js/FormJS.js
@@ -1,9 +1,4 @@
 $(document).ready(function(){
-$(".Ing").click(function(event){
-       var IngrName = this.val();
-     var all="<tr> <td class=\"NameIngr\"> <input type=\"text\" value="+IngrName+" class=\"TextNameIngr\"></td><td class=\"MassIngr\"><input type=\"text\" value=\"100гр\" class=\"TextMassIngr\">  </td> <td class=\"PrizeIngr\">  <input type=\"text\" value=\"200грн\"  class=\"TextPrizeIngr\"> </td></tr>";
-      $('#Choosen').append(all); 
-    });
 //Тут відбуваються дії при натисканні кнопки додати свою страву на сторінці productForm
 $(".Ad").click(function(){
       
@@ -11,4 +6,10 @@ $(".Ad").click(function(){
 $(".Back").click(function(){
     
 });
+  $(".AdIngridians").click(function(){
+    var IngrName=document.getElementById("NameOfIngrst").value;
+       var all="<tr> <td class=\"NameIngr\"> <input type=\"text\" value=\""+IngrName+"\" class=\"TextNameIngr\" ></td><td class=\"MassIngr\"><input type=\"text\" value=\"\" class=\"TextMassIngr\"required>  </td> <td class=\"PrizeIngr\">  <input type=\"text\" value=\"\"  class=\"TextPrizeIngr\" required> </td></tr>";
+      $('#Choosen').append(all);   
+      alert("Введіть будь ласка масу та ціну в форму!")
+});  
 });
