bb1cb197d112ced7752915557c42446f43462681
Solomia2018 <solomia2018@gmail.com>
2017-04-23 18:02:07 +0300

    Update FormJS.js

--- src/main/webapp/js/FormJS.js
+++ src/main/webapp/js/FormJS.js
@@ -8,7 +8,7 @@ $(".Back").click(function(){
 });
   $(".AdIngridians").click(function(){
     var IngrName=document.getElementById("NameOfIngrst").value;
-       var all="<tr> <td class=\"NameIngr\"> <input type=\"text\" value=\""+IngrName+"\" class=\"TextNameIngr\" ></td><td class=\"MassIngr\"><input type=\"text\" value=\"\" class=\"TextMassIngr\"required>  </td> <td class=\"PrizeIngr\">  <input type=\"text\" value=\"\"  class=\"TextPrizeIngr\" required> </td></tr>";
+       var all="<tr> <td class=\"NameIngr\"> <input type=\"text\" value=\""+IngrName+"\" class=\"TextNameIngr\" ></td><td class=\"MassIngr\"><input type=\"text\" value=\"\" class=\"TextMassIngr\"required>  </td><td class=\"MassBruttoIngr\"><input type=\"text\" value=\"\" class=\"TextNettoMassIngr\"required>  </td> <td class=\"PrizeIngr\">  <input type=\"text\" value=\"\"  class=\"TextPrizeIngr\" required> </td></tr>";
       $('#Choosen').append(all);   
       alert("Введіть будь ласка масу та ціну в форму!")
 });  
